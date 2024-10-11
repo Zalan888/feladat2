aa5e4f87343c7401111210f37659a82d077c08bc
Zalan888 <witmannz@kkszki.hu>
2024-10-11 12:15:31 +0200

    added Money def

--- Program.cs
+++ Program.cs
@@ -21,9 +21,27 @@ namespace udvozol
             else if (DateTime.Now.Hour <= 24 && DateTime.Now.Hour >= 20) { }
             return "a";
         }
+
+        // feladat2
+        public static int Money()
+        {
+            Console.WriteLine("Kérem adja meg a futamidőt");
+            int years = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérem adja meg a kezdőtőkét!");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérem adja meg a kamatot (százalék formájában)");
+            double interest = Convert.ToDouble(Console.ReadLine()) / 100;
+            double totalAmount = amount + (amount * interest * years);
+            return (int)totalAmount;
+        }
+
+        //feladat3
+
+
         static void Main(string[] args)
         {
             Console.WriteLine(Greeting());
+            Console.WriteLine(Money());
         }
     }
 }
